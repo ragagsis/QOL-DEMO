011431d2861a123dcca9ab390f5da403e73f1df3
ragagsis <7760@gsis.ac.in>
2024-08-05 18:30:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-document.getElementByName('First Name').value = location.pathname;
-document.getElementById('First Name').value = document.referrer;
+const hash = '?First Name=some_text&email=more%20text';
+const example = "https:/qol-demo.rgserver.in/form.html" + hash;
+
+(new URL(example)).searchParams.forEach((x, y) =>
+    document.getElementById(y).value = x);
